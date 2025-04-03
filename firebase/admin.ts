9d9c6933ef1a06b2bced7efcc0dd07701016bd75
Mohammed Farook Imran <farookimran54134@gmail.com>
2025-04-03 16:20:05 +0530

    admin

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,14 +1,14 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-
+import admin from "firebase-admin";
 // Initialize Firebase Admin SDK
 function initFirebaseAdmin() {
   const apps = getApps();
 
   if (!apps.length) {
     initializeApp({
-      credential: cert({
+      credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // Replace newlines in the private key
