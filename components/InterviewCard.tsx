51a2428140532f9c727a8c184c64c1888272726f
Mohammed Farook Imran <farookimran54134@gmail.com>
2025-04-04 13:03:17 +0530

    feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,9 +6,10 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.action';
 
-const InterviewCard = ({id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+    const feedback = userId && id ? await getFeedbackByInterviewId({interviewId:id,userId}):null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format("MM,DD,YYYY");
 
