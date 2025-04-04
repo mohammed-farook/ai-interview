d36e9968524c8a7c1d67bc3c0b695bd9cd0cba51
Mohammed Farook Imran <farookimran54134@gmail.com>
2025-04-04 12:02:03 +0530

    interview

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -90,36 +90,35 @@ export async function getFeedbackByInterviewId(
   return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
 }
 
-// export async function getLatestInterviews(
-//   params: GetLatestInterviewsParams
-// ): Promise<Interview[] | null> {
-//   const { userId, limit = 20 } = params;
+export async function getInterviewsByUserId(userId:string):Promise<Interview[] | null> {
+  const interviews = await db
+  .collection('interviews')
+  .where('userId', '==', userId)
+  .orderBy('createdAt','desc')
+  .get();
 
-//   const interviews = await db
-//     .collection("interviews")
-//     .orderBy("createdAt", "desc")
-//     .where("finalized", "==", true)
-//     .where("userId", "!=", userId)
-//     .limit(limit)
-//     .get();
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+   ...doc.data() 
+  })) as Interview[];
+}
 
-//   return interviews.docs.map((doc) => ({
-//     id: doc.id,
-//     ...doc.data(),
-//   })) as Interview[];
-// }
+  export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
 
-// export async function getInterviewsByUserId(
-//   userId: string
-// ): Promise<Interview[] | null> {
-//   const interviews = await db
-//     .collection("interviews")
-//     .where("userId", "==", userId)
-//     .orderBy("createdAt", "desc")
-//     .get();
+  const interviews = await db
+    .collection("interviews")
+    .where("finalized", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(limit)
+    .get();
 
-//   return interviews.docs.map((doc) => ({
-//     id: doc.id,
-//     ...doc.data(),
-//   })) as Interview[];
-// }
+  return interviews.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .filter((interview) => interview.userId !== userId) as Interview[];
+}
